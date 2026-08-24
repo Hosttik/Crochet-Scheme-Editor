f77d7a8f29287a8cf9da1a831a5e1db84cccda45
Hosttik <raz_94@mail.ru>
2026-08-24 15:11:01 +0200

    Load selection interaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './guides.css'
+import './selection.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
