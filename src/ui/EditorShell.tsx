d73572a65193baf72a4e16fbcf93898e7f064c2c
Hosttik <raz_94@mail.ru>
2026-08-28 17:51:52 +0200

    Mount command palette in editor shell

--- src/ui/EditorShell.tsx
+++ src/ui/EditorShell.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react'
 import { AppMenuBar } from './AppMenuBar'
+import { CommandPalette } from './CommandPalette'
 import { LeftWorkbenchBridge } from './LeftWorkbenchBridge'
 import { RightInspectorBridge } from './RightInspectorBridge'
 import './editor-shell.css'
@@ -8,6 +9,7 @@ export function EditorShell({ children }: { children: ReactNode }) {
   return (
     <div className="editor-root-v2">
       <AppMenuBar />
+      <CommandPalette />
       <div className="editor-root-v2__workbench">
         {children}
         <LeftWorkbenchBridge />
