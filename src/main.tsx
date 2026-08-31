57c93790d2f6cf9c4a65b0048183af89df919e55
Hosttik <raz_94@mail.ru>
2026-08-31 18:48:16 +0200

    Load compact chrome overrides last

--- src/main.tsx
+++ src/main.tsx
@@ -21,6 +21,7 @@ import './ui/appVersionBadge.css'
 import './ui/leftWorkbenchReference.css'
 import './ui/canvasAuthoringV2.css'
 import './ui/authoringUsabilityV1268.css'
+import './ui/compactChromeV12610.css'
 
 const rootElement = document.getElementById('root')!
 rootElement.style.setProperty('--app-version-label', `"v${packageJson.version}"`)
