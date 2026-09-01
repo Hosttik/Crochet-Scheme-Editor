c30274f60b7adb4990dc961aa11eaa86a3bc8fa7
Hosttik <raz_94@mail.ru>
2026-09-01 17:17:50 +0200

    ux: reveal properties when selection changes

--- src/ui/SelectionInspector.tsx
+++ src/ui/SelectionInspector.tsx
@@ -1,8 +1,15 @@
-import type { ComponentProps } from 'react'
+import { useEffect, type ComponentProps } from 'react'
 import { FanGeometryControl } from '../editor/FanGeometryControl'
 import { SelectionInspector as SelectionInspectorCore } from './SelectionInspectorCore'
+import { setRightPanelMode } from './RightPanelTabs'
 
 export function SelectionInspector(props: ComponentProps<typeof SelectionInspectorCore>) {
+  const hasContextSelection = props.selectedIds.length > 0 || Boolean(props.selectedGuide)
+
+  useEffect(() => {
+    if (hasContextSelection) setRightPanelMode('properties')
+  }, [hasContextSelection, props.selectedElement?.id, props.selectedGuide?.id])
+
   return (
     <div className="right-properties-context right-properties-selection" data-testid="right-properties-selection">
       <SelectionInspectorCore {...props} />
